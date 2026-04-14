de1f45b478f8fa7e757d24fd756722fa598410d7
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2026-04-13 22:25:44 -0600

    Addition of custom properties

--- spring/1-spring-boot-setup/src/main/java/com/spring/demo/demo/rest/HelloWorldController.java
+++ spring/1-spring-boot-setup/src/main/java/com/spring/demo/demo/rest/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.spring.demo.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,11 @@ import com.spring.demo.demo.playground.LombokData;
 
 @RestController
 public class HelloWorldController {
+    @Value("${dog.name}")
+    private String dogName;
+
+    @Value("${cat.name}")
+    private String catName;
 
     // expose '/' that return "Hello World"
     @GetMapping("/")
@@ -14,6 +20,11 @@ public class HelloWorldController {
         LombokData lombok = new LombokData();
         lombok.setMessage("Lombok is working!!!!");
         return lombok.getMessage();
+    }
+
+    @GetMapping("/custom")
+    public String custom() {
+        return "Dog name: " + dogName + " Cat name: " + catName;
     }
 
 }
