22e6538f467815bac0619812edf5633eb263688c
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2026-04-13 20:38:04 -0600

    Added dev tools

---
+++ spring/1-spring-boot-setup/src/main/java/com/spring/demo/demo/playground/LombokData.java
@@ -0,0 +1,8 @@
+package com.spring.demo.demo.playground;
+
+import lombok.Data;
+
+@Data
+public class LombokData {
+    private String message;
+}
--- spring/1-spring-boot-setup/src/main/java/com/spring/demo/demo/rest/HelloWorldController.java
+++ spring/1-spring-boot-setup/src/main/java/com/spring/demo/demo/rest/HelloWorldController.java
@@ -1,7 +1,9 @@
 package com.spring.demo.demo.rest;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.demo.demo.playground.LombokData;
 
 @RestController
 public class HelloWorldController {
@@ -9,7 +11,9 @@ public class HelloWorldController {
     // expose '/' that return "Hello World"
     @GetMapping("/")
     public String hello() {
-        return "Hello World";
+        LombokData lombok = new LombokData();
+        lombok.setMessage("Lombok is working!!!!");
+        return lombok.getMessage();
     }
 
 }
