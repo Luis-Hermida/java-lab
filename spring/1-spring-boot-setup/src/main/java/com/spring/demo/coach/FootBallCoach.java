6e1394092b1c2498f3976060402c8271b340132f
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2026-04-22 23:27:35 -0600

    Tested out beans lifecycle methods

--- spring/1-spring-boot-setup/src/main/java/com/spring/demo/coach/FootBallCoach.java
+++ spring/1-spring-boot-setup/src/main/java/com/spring/demo/coach/FootBallCoach.java
@@ -3,12 +3,27 @@ package com.spring.demo.coach;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 @Primary
 public class FootBallCoach implements Coach {
 
     public FootBallCoach() {
         System.out.println("Constructor info: " + getClass().getSimpleName());
+    }
+
+    // define initialization method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("Inside method: doMyStartupStuff " + getClass().getSimpleName());
+    }
+
+    // define destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("Inside method: doMyCleanupStuff " + getClass().getSimpleName());
     }
 
     @Override
