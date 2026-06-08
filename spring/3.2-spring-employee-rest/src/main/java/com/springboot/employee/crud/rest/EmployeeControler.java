045427dc59fe760f9bdb4fd932108c61ea5206de
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2026-06-07 21:29:15 -0600

    Added employee service

--- spring/3.2-spring-employee-rest/src/main/java/com/springboot/employee/crud/rest/EmployeeControler.java
+++ spring/3.2-spring-employee-rest/src/main/java/com/springboot/employee/crud/rest/EmployeeControler.java
@@ -6,22 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.springboot.employee.crud.dao.EmployeeDAO;
 import com.springboot.employee.crud.entity.Employee;
+import com.springboot.employee.crud.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeControler {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
-    public EmployeeControler(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeControler(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping("employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 
 }
---
+++ spring/3.2-spring-employee-rest/src/main/java/com/springboot/employee/crud/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.springboot.employee.crud.service;
+
+import java.util.List;
+
+import com.springboot.employee.crud.entity.Employee;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+}
