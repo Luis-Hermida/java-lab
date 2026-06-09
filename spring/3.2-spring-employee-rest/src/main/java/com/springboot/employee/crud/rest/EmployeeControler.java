e8fd12d79e0bbb79e80af182d4bd6542386b90d5
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2026-06-09 00:45:15 -0600

    Added POST /employees

--- spring/3.2-spring-employee-rest/src/main/java/com/springboot/employee/crud/rest/EmployeeControler.java
+++ spring/3.2-spring-employee-rest/src/main/java/com/springboot/employee/crud/rest/EmployeeControler.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,20 +22,29 @@ public class EmployeeControler {
         this.employeeService = employeeService;
     }
 
-    @GetMapping("employees")
+    @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
     }
 
-    @GetMapping("employees/{employeeId}")
+    @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId) {
-        Employee employee = employeeService.findById(employeeId);
+        Employee dbEmployee = employeeService.findById(employeeId);
 
-        if (employee == null) {
+        if (dbEmployee == null) {
             throw new RuntimeException("Employee id not found - " + employeeId);
         }
 
-        return employee;
+        return dbEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+
+        Employee dbEmployee = employeeService.save(employee);
+
+        return dbEmployee;
     }
 
 }
