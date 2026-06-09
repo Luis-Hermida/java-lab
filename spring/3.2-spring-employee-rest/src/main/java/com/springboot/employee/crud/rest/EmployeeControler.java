de7e2fffbf754e27c6b6a1eed09b7d1f5dc387c4
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2026-06-09 05:58:53 -0600

    Added PUT /employees

--- spring/3.2-spring-employee-rest/src/main/java/com/springboot/employee/crud/rest/EmployeeControler.java
+++ spring/3.2-spring-employee-rest/src/main/java/com/springboot/employee/crud/rest/EmployeeControler.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,13 @@ public class EmployeeControler {
     public Employee addEmployee(@RequestBody Employee employee) {
         employee.setId(0);
 
+        Employee dbEmployee = employeeService.save(employee);
+
+        return dbEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
         Employee dbEmployee = employeeService.save(employee);
 
         return dbEmployee;
