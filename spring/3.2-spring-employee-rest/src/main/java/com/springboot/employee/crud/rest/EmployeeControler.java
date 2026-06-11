e45b1d2fefbf5e524127f4869fc8595dac379368
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2026-06-10 19:25:37 -0600

    Added DELETE /employees/;id

--- spring/3.2-spring-employee-rest/src/main/java/com/springboot/employee/crud/rest/EmployeeControler.java
+++ spring/3.2-spring-employee-rest/src/main/java/com/springboot/employee/crud/rest/EmployeeControler.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -80,6 +81,19 @@ public class EmployeeControler {
         Employee dbEmployee = employeeService.save(patchedEmployee);
 
         return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
     }
 
 }
