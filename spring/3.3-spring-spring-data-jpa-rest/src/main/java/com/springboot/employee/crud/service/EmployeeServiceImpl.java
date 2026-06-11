6787f375ed28738065c9ab240fdf384484a8018c
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2026-06-10 19:34:21 -0600

    /spring/3.3 project initialization

---
+++ spring/3.3-spring-spring-data-jpa-rest/src/main/java/com/springboot/employee/crud/service/EmployeeServiceImpl.java
@@ -0,0 +1,43 @@
+package com.springboot.employee.crud.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.springboot.employee.crud.dao.EmployeeDAO;
+import com.springboot.employee.crud.entity.Employee;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int Id) {
+        return employeeDAO.findById(Id);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int Id) {
+        employeeDAO.deleteById(Id);
+    }
+
+}
