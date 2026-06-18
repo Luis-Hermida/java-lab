cfcb5b28521c1dd39f603aa68f7bfd85b62f0f67
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2026-06-17 23:12:46 -0600

    Changed basic configuration

--- spring/3.4-spring-data-rest/src/main/java/com/springboot/employee/crud/dao/EmployeeRepository.java
+++ spring/3.4-spring-data-rest/src/main/java/com/springboot/employee/crud/dao/EmployeeRepository.java
@@ -1,9 +1,10 @@
 package com.springboot.employee.crud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.springboot.employee.crud.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
 }
