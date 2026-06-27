a949539c028be82bb7110979664f2a01f9a15d65
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2026-06-27 15:48:18 -0600

    Replaced hardcoded userDetails to use db values

--- spring/4-spring-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ spring/4-spring-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,37 +1,22 @@
 package com.luv2code.springboot.cruddemo.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
         @Bean
-        public InMemoryUserDetailsManager userDetailsManager() {
-                UserDetails james = User.builder()
-                                .username("james")
-                                .password("{noop}test123")
-                                .roles("EMPLOYEE")
-                                .build();
-                UserDetails mary = User.builder()
-                                .username("mary")
-                                .password("{noop}test123")
-                                .roles("EMPLOYEE", "MANAGER")
-                                .build();
-                UserDetails maria = User.builder()
-                                .username("maria")
-                                .password("{noop}test123")
-                                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                                .build();
-
-                return new InMemoryUserDetailsManager(james, mary, maria);
+        public UserDetailsManager userDetailsManager(DataSource dataSource) {
+                return new JdbcUserDetailsManager(dataSource);
         }
 
         @Bean
