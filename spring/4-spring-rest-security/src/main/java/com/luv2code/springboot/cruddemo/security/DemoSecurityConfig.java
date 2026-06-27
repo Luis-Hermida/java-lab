e5585b3aa31262bb994ec59e487819f2353e2c13
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2026-06-27 12:41:49 -0600

    Restricted URL access based on role

--- spring/4-spring-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ spring/4-spring-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,32 +2,60 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails james = User.builder()
-                .username("james")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
+        @Bean
+        public InMemoryUserDetailsManager userDetailsManager() {
+                UserDetails james = User.builder()
+                                .username("james")
+                                .password("{noop}test123")
+                                .roles("EMPLOYEE")
+                                .build();
+                UserDetails mary = User.builder()
+                                .username("mary")
+                                .password("{noop}test123")
+                                .roles("EMPLOYEE", "MANAGER")
+                                .build();
+                UserDetails maria = User.builder()
+                                .username("maria")
+                                .password("{noop}test123")
+                                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                                .build();
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-        UserDetails maria = User.builder()
-                .username("maria")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
+                return new InMemoryUserDetailsManager(james, mary, maria);
+        }
 
-        return new InMemoryUserDetailsManager(james, mary, maria);
-    }
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+                httpSecurity.authorizeHttpRequests(configurer -> configurer
+                                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
+
+                // Use HTTP basic configuration
+                httpSecurity.httpBasic(Customizer.withDefaults());
+
+                // Disable Cross Site Request Forgery (CSRF)
+                // Not required for stateless REST API's
+                // -- Used for HTML applications
+                httpSecurity.csrf(csrf -> csrf.disable());
+
+                // Don't use sessions, each request makes their own
+                // httpSecurity.sessionManagement(
+                // session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+                return httpSecurity.build();
+        }
 }
