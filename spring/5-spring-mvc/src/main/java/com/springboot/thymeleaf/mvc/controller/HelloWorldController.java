da0c5b4fac28f5fe025cde787c17c76d4b3a7e5b
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2026-07-04 20:47:46 -0600

    Added data to model based on the request

--- spring/5-spring-mvc/src/main/java/com/springboot/thymeleaf/mvc/controller/HelloWorldController.java
+++ spring/5-spring-mvc/src/main/java/com/springboot/thymeleaf/mvc/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.springboot.thymeleaf.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -14,6 +17,24 @@ public class HelloWorldController {
     // Controller method to process HTML form
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld-result";
+    }
+
+    // Controller method to process HTML form
+    @RequestMapping("/processFormAllCaps")
+    public String processFormAllCaps(HttpServletRequest request, Model model) {
+        // read te request parameter
+        String name = request.getParameter("studentName");
+
+        // convert data
+        name = name.toUpperCase();
+
+        // create message
+        String message = "YOOO!!! " + name;
+
+        // add message to model
+        model.addAttribute("message", message);
+
         return "helloworld-result";
     }
 }
