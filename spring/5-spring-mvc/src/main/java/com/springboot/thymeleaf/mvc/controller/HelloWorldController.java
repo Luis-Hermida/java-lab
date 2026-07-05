273e90a31d0b390d5a466ca19158e7c47f05a9fc
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2026-07-04 20:54:54 -0600

    Added binding requests params

--- spring/5-spring-mvc/src/main/java/com/springboot/thymeleaf/mvc/controller/HelloWorldController.java
+++ spring/5-spring-mvc/src/main/java/com/springboot/thymeleaf/mvc/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.springboot.thymeleaf.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -14,15 +15,15 @@ public class HelloWorldController {
         return "helloworld-form";
     }
 
-    // Controller method to process HTML form
+    // Basic controller method to process HTML form
     @RequestMapping("/processForm")
     public String processForm() {
         return "helloworld-result";
     }
 
-    // Controller method to process HTML form
-    @RequestMapping("/processFormAllCaps")
-    public String processFormAllCaps(HttpServletRequest request, Model model) {
+    // Controller method that adds model data
+    @RequestMapping("/processFormTwo")
+    public String processFormTwo(HttpServletRequest request, Model model) {
         // read te request parameter
         String name = request.getParameter("studentName");
 
@@ -31,6 +32,21 @@ public class HelloWorldController {
 
         // create message
         String message = "YOOO!!! " + name;
+
+        // add message to model
+        model.addAttribute("message", message);
+
+        return "helloworld-result";
+    }
+
+    // Controller method that uses request params
+    @RequestMapping("/processFormThree")
+    public String processFormThree(@RequestParam("studentName") String name, Model model) {
+        // convert data
+        name = name.toUpperCase();
+
+        // create message
+        String message = "Hi my beloved " + name;
 
         // add message to model
         model.addAttribute("message", message);
